e5ee11701c53b5202efbdc851fdd0b18a0eeb933
anuj-tambwekar <anuj@vijil.ai>
2025-08-29 20:41:37 -0400

    add latency param when using evaluation to create dome (#30)
    Add the latency parameter when creating configs from evals

--- vijil_dome/integrations/vijil/evaluate.py
+++ vijil_dome/integrations/vijil/evaluate.py
@@ -3,7 +3,7 @@ Integrations with Vijil Evaluate
 """
 
 import httpx
-from typing import Optional
+from typing import Optional, Dict, Any # noqa: F401
 
 VIJIL_API_BASE_URL = "https://evaluate-api.vijil.ai/v1"
 
@@ -49,7 +49,10 @@ def get_config_from_vijil_agent(
 
 
 def get_config_from_vijil_evaluation(
-    api_token: str, evaluation_id: str, base_url: Optional[str] = None
+    api_token: str,
+    evaluation_id: str,
+    base_url: Optional[str] = None,
+    latency_threshold: Optional[float] = None,
 ) -> Optional[dict]:
     """
     Fetch the Dome configuration from a specific evaluation in Vijil Evaluate using the provided API token and evaluation ID.
@@ -71,7 +74,9 @@ def get_config_from_vijil_evaluation(
     base_url = base_url or VIJIL_API_BASE_URL
     url = f"{base_url}/recommend-dome-config"
 
-    payload = {"evaluation_id": evaluation_id}
+    payload = {"evaluation_id": evaluation_id} # type: Dict[str, Any]
+    if latency_threshold:
+        payload["latency_threshold"] = latency_threshold
 
     try:
         response = httpx.post(url, headers=headers, json=payload)
